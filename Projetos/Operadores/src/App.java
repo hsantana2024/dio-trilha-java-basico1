e15b6c8d8ca01e6a8485464f7bbd57a90303e58f
Humberto Santana <167917478+hsantana2024@users.noreply.github.com>
2024-04-30 11:17:39 -0300

    teste

---
+++ Projetos/Operadores/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        
+        numero =  numero * -1;
+
+        System.out.println(numero);
+
+        
+
+    }
+
+
+
+
+
+       }
+
